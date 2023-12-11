e7bceec11168b614cf2ada504e9eb438cce38537
Alexander-Infante <alexinfante17@gmail.com>
2023-12-10 17:14:09 -0800

    Quick hacking together

---
+++ script.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const bucketUrl = "https://ariana-ava-wedding-2023.s3.amazonaws.com/";
+
+    fetch(bucketUrl)
+        .then(response => response.text())
+        .then(data => {
+            const parser = new DOMParser();
+            const xmlDoc = parser.parseFromString(data, "text/xml");
+            const files = xmlDoc.getElementsByTagName("Contents");
+
+            for (let i = 0; i < files.length; i++) {
+                const key = files[i].getElementsByTagName("Key")[0].childNodes[0].nodeValue;
+
+                if (key.endsWith(".jpg") || key.endsWith(".png")) {
+                    const container = document.createElement("div");
+                    container.className = "image-container";
+
+                    const img = document.createElement("img");
+                    img.src = bucketUrl + key;
+                    container.appendChild(img);
+                    img.onclick = function() {
+                        openModal(this.src);
+                    };
+
+                    document.getElementById("gallery").appendChild(container);
+                } 
+                // TODO PDF
+                // else if (key.endsWith(".pdf")) {
+                //     const link = document.createElement("a");
+                //     link.href = bucketUrl + key;
+                //     link.textContent = "PDF: " + key;
+                //     link.className = "pdf-link";
+                //     document.getElementById("gallery").appendChild(link);
+                // }
+            }
+        })
+        .catch(error => console.log("Error fetching and parsing data:", error));
+
+    // Function to open modal
+    function openModal(src) {
+        const modal = document.getElementById("myModal");
+        const modalImg = document.getElementById("img01");
+
+        modal.style.display = "block";
+        modalImg.src = src;
+
+        // Get the <span> element to close the modal
+        const span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() { 
+            modal.style.display = "none";
+        }
+    }
+});
